d390a97fd063db8dad39c1cab6585e02d14fb7aa
Johannes Sarpola <johannes.sarpola@gmail.com>
2024-12-14 15:34:11 +0200

    fix test

--- node_test.go
+++ node_test.go
@@ -2,22 +2,24 @@ package gollections
 
 import (
 	"testing"
+
+	"github.com/johannessarpola/gollections/optional"
 )
 
 func TestNode(t *testing.T) {
 	type testCase[T comparable] struct {
 		name    string
 		in      T
-		prev    Optional[T]
-		next    Optional[T]
+		prev    optional.Optional[T]
+		next    optional.Optional[T]
 		want    T
 		wantStr string
 	}
 	intTests := []testCase[int]{
 		{name: "node-1", in: 1, want: 1, wantStr: "1"},
 		{name: "node-2", in: -13, want: -13, wantStr: "-13"},
-		{name: "node-3", in: 3, want: 3, wantStr: "3", next: NewExistingOptional(4)},
-		{name: "node-4", in: 4, want: 4, wantStr: "4", prev: NewExistingOptional(3)},
+		{name: "node-3", in: 3, want: 3, wantStr: "3", next: optional.NewExisting(4)},
+		{name: "node-4", in: 4, want: 4, wantStr: "4", prev: optional.NewExisting(3)},
 	}
 	for _, tt := range intTests {
 		t.Run(tt.name, func(t *testing.T) {
